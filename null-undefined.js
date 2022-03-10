bc58390a20e14ed3dfa3ed14840b34c67b1a6ee6
laskor <al.moksud@gmail.com>
2022-03-09 20:18:09 -0500

    null

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+
+// null vs undefined
+
+/* undefined
+>> variable value not assigned
+>> function but didn't write return 
+>> wrote return but didn't return anything
+>> parameter that isn't passed
+>> property that dosen't exists in a object
+
+*/
+
+let First;
+function second(x, y){
+    const sum = x + y;
+    return;
+    return fun = a +blur;
+    return sum;
+}
+const result = second(3, 91);
+console.log(result);
