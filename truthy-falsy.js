d15d9b78dee49807b2fa878e4a2ee2c2a195b79e
laskor <al.moksud@gmail.com>
2022-03-09 20:05:20 -0500

    truthy-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// truthy & falsy js
+
+// falsy
+/*  false
+    any negative number including 0
+    empty string
+
+*/
+const x = false;
+if(x){
+    console.log('is truthy');
+}
+else{
+    console.log('falsy');
+}
+
+// truthy
+/*  true
+    any positive number without 0
+    any string including any single whitespace
+*/
+const y = true;
+if(y){
+    console.log('is truthy');
+}
+else{
+    console.log('falsy');
+}
