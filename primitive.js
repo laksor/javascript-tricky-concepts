a7a649dae07e90d4f59f5f24e61523be901b485d
laskor <al.moksud@gmail.com>
2022-03-09 19:55:52 -0500

    pri-no-primitive data types

--- primitive.js
+++ primitive.js
@@ -1,3 +1,17 @@
 
-// data types primitive
+// 6 data types are primitive - means ( single value )
 
+/* 7 data types
+1. number
+2.string
+3.boolean
+4. undefined
+5.null.
+7.symbol
+
+// non-primitive are :
+
+6.object
+
+
+*/
