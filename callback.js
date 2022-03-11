e3d217e0919e56b2704d1e031c863315d7343c92
laskor <al.moksud@gmail.com>
2022-03-10 19:47:03 -0500

    callback function

--- callback.js
+++ callback.js
@@ -1,7 +1,13 @@
 // Callback function and pass different functions
 
-function welcomeMassage(name){
-    console.log(name);
+function welcomeMassage(name,geetHandler){
+    geetHandler(name);
 }
-const name = 'tom'
-welcomeMassage(name);
+// const name = ['tom', 'lala', 'mama'];
+// const obj = {name: 'tom', age: 11};
+
+function greet(name){
+    console.log('morning', name);
+}
+
+welcomeMassage('tom', greet);
